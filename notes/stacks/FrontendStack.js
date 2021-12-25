007380cf05f7aeed3789f95606bdef31d2cea441
Owen McCadden <owenmc@live.unc.edu>
2021-12-24 19:26:37 -0500

    react app setup

---
+++ notes/stacks/FrontendStack.js
@@ -0,0 +1,26 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class FrontendStack extends sst.Stack {
+    constructor(scope, id, props) {
+        super(scope, id, props);
+
+        const {api, auth, bucket} = props;
+
+        const site = new sst.ReactStaticSite(this, "ReactSite", {
+            path: "frontend",
+
+            environment: {
+                REACT_APP_API_URL: api.url,
+                REACT_APP_REGION: scope.region,
+                REACT_APP_BUCKET: bucket.bucketName,
+                REACT_APP_USER_POOL_ID: auth.cognitoUserPool.userPoolId,
+                REACT_APP_IDENTITY_POOL_ID: auth.cognitoCfnIdentityPool.ref,
+                REACT_APP_USER_POOL_CLIENT_ID: auth.cognitoUserPoolClient.userPoolClientId,
+            },
+        });
+
+        this.addOutputs({
+            SiteUrl: site.url,
+        });
+    }
+}
