f35e4fec2e168b4b0036809b2a3c4851030069e7
Owen McCadden <owenmc@live.unc.edu>
2021-12-24 22:53:26 -0500

    migrate to react router v6 and added amplify config

--- notes/frontend/src/App.js
+++ notes/frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useNavigate } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './containers/Home';
 import './App.css';
-
-import { Button } from "react-bootstrap";
+import NotFound from "./containers/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg='light' expand="md" className="mb-3">
+          <Navbar.Brand className="font-weight-bold text-muted" as={Link} to="/">
+            Scratch
+          </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/signup" element={<Home/>}/>
+            <Route path="/login" element={<Home/>}/>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
     </div>
   );
 }
---
+++ notes/frontend/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeStyles.css";
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Scratch</h1>
+                <p className="text-muted">A simple note taking app</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home
